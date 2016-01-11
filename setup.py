64b67911576751c4b596242928e8f338f5036ed0
Anthony Grimes <i@raynes.me>
2016-01-10 20:40:04 -0800

    Increment version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='rxvc',
     description=__doc__,
-    version='0.1.0',
+    version='0.1.1',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,

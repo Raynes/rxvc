d0444d1fde5c5b3c4d9a90503a126082a7e616ee
Anthony Grimes <i@raynes.me>
2016-01-10 17:50:09 -0800

    Document the cache module.

--- rxvc/cache.py
+++ rxvc/cache.py
@@ -1,3 +1,4 @@
+"""Functions for working with our receiver cache."""
 import json
 from pathlib import Path
 from os.path import expanduser
@@ -7,6 +8,11 @@ cache_path = Path(expanduser("~/.rxvc_cache"))
 
 
 def cache_receiver(receiver):
+    """Touch our cache file to make sure it at least exists and
+    dump what we know (control url, name, model) to json into
+    the file as a cache for the next run of this project.
+    
+    """
     cache_path.touch(exist_ok=True)
     cache_path.write_text(json.dumps({
         'ctrl_url': receiver.ctrl_url,
@@ -15,6 +21,11 @@ def cache_receiver(receiver):
 
 
 def cached_receiver():
+    """Return an RXV object for our cached receiver if there
+    is one otherwise return None and the cache will be populated
+    on the next run of rxvc.
+    
+    """
     if cache_path.exists():
         parsed_cache = json.loads(cache_path.read_text())
         return RXV(parsed_cache['ctrl_url'],
@@ -23,5 +34,6 @@ def cached_receiver():
 
 
 def clear():
+    """Clear the receiver cache if it exists."""
     if cache_path.exists():
         cache_path.unlink()
